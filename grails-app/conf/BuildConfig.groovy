332f8f333b1860fb7510fbca843c751b4b403ff0
Alex Abd <Alexander.Abdugafarov@nitka.com>
2012-12-07 02:19:31 +0500

    Reverted last change

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -39,7 +39,7 @@ grails.project.dependency.resolution = {
 	}
 
 	plugins {
-		build(":release:2.2.0", ":rest-client-builder:1.0.3") {
+		build(":release:2.1.0", ":rest-client-builder:1.0.2") {
 			export = false
 		}
 
