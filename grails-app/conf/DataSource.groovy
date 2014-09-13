bc7b8fac146bff777cd25bd6316ff7a0e6f3f7bf
Damir Murat <dmurat@velim.sx76x.gigaset.net>
2014-09-14 00:19:47 +0200

    data source config updated

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -7,30 +7,40 @@ dataSource {
 
 hibernate {
     cache.use_second_level_cache = true
-    cache.use_query_cache = true
-    cache.provider_class = 'net.sf.ehcache.hibernate.EhCacheProvider'
+    cache.use_query_cache = false
+    cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory'
 }
 
-// environment specific settings
 environments {
     development {
         dataSource {
             dbCreate = "create-drop"
-            url = "jdbc:h2:mem:devDb"
+            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
         }
     }
 
     test {
         dataSource {
-            dbCreate = "create-drop"
-            url = "jdbc:h2:mem:testDb"
+            dbCreate = "update"
+            url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
         }
     }
 
     production {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:h2:prodDb"
+            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+            pooled = true
+            properties {
+                maxActive = -1
+                minEvictableIdleTimeMillis=1800000
+                timeBetweenEvictionRunsMillis=1800000
+                numTestsPerEvictionRun=3
+                testOnBorrow=true
+                testWhileIdle=true
+                testOnReturn=true
+                validationQuery="SELECT 1"
+            }
         }
     }
 }
